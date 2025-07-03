a6967d49dc62acc081e87cf266aba4658a5402d2
Webaccount284 <amazspidy284@gmail.com>
2025-07-03 19:39:13 +0100

    DUFNSA

--- MCRPE Source Code/Program.cs
+++ MCRPE Source Code/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using system;
+
+namespace MCRPE_source_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!")
+        }
+    }
+}
