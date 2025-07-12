0d9c6d8efc0ba9982edeacf34db41bdec3fbb200
Webaccount284 <amazspidy284@gmail.com>
2025-07-12 16:43:06 +0100

    eedb

--- FormGame/obj/Debug/net8.0-windows/FormGame.AssemblyInfo.cs
+++ FormGame/obj/Debug/net8.0-windows/FormGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51638c0a60fcbffb0c17fdb1b298680c9647967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73345b0f27a41d593bb9d4afee9b20910be31217")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
